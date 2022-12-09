6eb0ea8acb665a895a0679cc117d61a7c6da9d60
leomiranda-tc <leonardo.miranda@tc.com.br>
2022-12-09 17:23:16 -0300

    feat: database queries

--- assets/1670617325_data2.sql
+++ assets/1670617325_data2.sql
@@ -1,7 +1,7 @@
 --migrate-up--
-CREATE TABLE nada(
+CREATE TABLE nada2(
     id int auto_increment not null primary key
 )
 
 --migrate-down--
-DROP TABLE nada
+DROP TABLE nada2
--- internal/database/sql_impl.go
+++ internal/database/sql_impl.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"errors"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -11,32 +12,97 @@ type implDatabase struct {
 	conn *sql.DB
 }
 
+const TABLE_MIGRATIONS = "migration_sql_db"
+
 func New(driver, connection string) IDatabase {
 	conn, err := sql.Open(driver, connection)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return &implDatabase{
+	db := &implDatabase{
 		conn: conn,
 	}
+
+	if err := db.createTableIfNotExists(); err != nil {
+		log.Fatal(err)
+	}
+
+	return db
 }
 
 func (d *implDatabase) GetLatestMigration() (string, error) {
-	return "1670612283_data", nil
+	var data string
+
+	d.conn.QueryRow(`
+		SELECT migration FROM ` + TABLE_MIGRATIONS + ` ORDER BY id DESC LIMIT 1
+	`).Scan(&data)
+
+	if data == "" {
+		return "", errors.New("error to get the latest migration")
+	}
+
+	return data, nil
 }
 
 func (d *implDatabase) GetAllMigrations() (map[string]bool, error) {
 	migrations := make(map[string]bool)
+
+	rows, err := d.conn.Query("SELECT migration FROM " + TABLE_MIGRATIONS)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var migrationName string
+		if err := rows.Scan(&migrationName); err != nil {
+			return nil, err
+		}
+
+		migrations[migrationName] = true
+	}
+
 	return migrations, nil
 }
 
 func (d *implDatabase) RunMigration(name, query string) error {
-	//run query and if its succeed, store on table migrations
+	_, err := d.conn.Exec(query)
+	if err != nil {
+		return err
+	}
+
+	_, err = d.conn.Exec(`
+		INSERT INTO `+TABLE_MIGRATIONS+` (migration) VALUES (?)
+	`, name)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (d *implDatabase) RunRollback(name, query string) error {
-	//run query and if its succeed, delete on table migrations
+	_, err := d.conn.Exec(query)
+	if err != nil {
+		return err
+	}
+
+	_, err = d.conn.Exec(`
+		DELETE FROM `+TABLE_MIGRATIONS+` WHERE migration = ?
+	`, name)
+	if err != nil {
+		return err
+	}
 	return nil
+}
+
+func (d *implDatabase) createTableIfNotExists() error {
+	_, err := d.conn.Exec(`
+		CREATE TABLE IF NOT EXISTS ` + TABLE_MIGRATIONS + ` (
+			id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			migration VARCHAR(100),
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		)
+	`)
+	return err
 }
